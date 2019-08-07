133b278353f60830b4b37047aaaa55ae48e37cf4
mavaleriano <miasvaleriano@gmail.com>
2019-08-07 10:50:55 -0400

    Final revision
    Everything seems to work except the resulting links from the search, although it's extra credit

--- js/script.js
+++ js/script.js
@@ -7,155 +7,171 @@ FSJS project 2 - List Filter and Pagination
 
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
+Calling global variables used in the program 
 ***/
-const allStudents = document.querySelectorAll('li');
-const pageDiv = document.querySelector('.page');
-const allNames = document.querySelectorAll('h3');
-const allLinks = document.getElementsByTagName('A');
-const header = document.querySelector('.page-header.cf');
-const search = document.createElement('div');
-const noResults = document.createElement('div');
-noResults.innerHTML = "<h3>No search results found..</h3>";
-search.className = "student-search";
-header.appendChild(search);
-const input = document.createElement('input');
-input.placeholder = "Search for students..";
-search.appendChild(input);
-const searchButton = document.createElement('button');
-searchButton.textContent = "Search";
-searchButton.className = "searching"
-search.appendChild(searchButton);
+document.addEventListener('DOMContentLoaded', () => { // Setting up to make sure script waits for content to load
+    const allStudents = document.querySelectorAll('li');
+    const pageDiv = document.querySelector('.page');
+    const allNames = document.querySelectorAll('h3');
+    const allLinks = document.getElementsByTagName('A');
+    const header = document.querySelector('.page-header.cf');
+    const search = document.createElement('div');
+    const noResults = document.createElement('div');
+    noResults.innerHTML = "<h2>No search results found..</h2>";
+    search.className = "student-search";
+    header.appendChild(search);
+    const input = document.createElement('input');
+    input.placeholder = "Search for students..";
+    search.appendChild(input);
+    const searchButton = document.createElement('button');
+    searchButton.textContent = "Search";
+    search.appendChild(searchButton);
+    const resetButton = document.createElement('button');
+    resetButton.textContent = "Reset";
+    search.appendChild(resetButton);
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+    /*** 
+        showPage takes the list of students and the page to show
+    ***/
+    const showPage = (list, page) => {
+        for (let i = 0; i < list.length; i += 1) {
+            let student = list[i];
+            let first = (page - 1) * 10;                //sets the first student that should show up
+            let last = page * 10;                       //sets last student that should show up
 
-***/
-const showPage = (list, page) =>
-{
-    for (let i = 0; i < list.length; i += 1)
-    {
-        let student = list[i];
-        let first = (page - 1) * 10; //sets the first student that should show up
-        let last = page * 10;        //sets last student that should show up
-      
-        if (i >= first && i < last)
-        {
-            student.style.display = "block";
-        }
-        else
-        {
-            student.style.display = "none";
-        }
-    }
-}
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-const appendPageLinks = (allStudents) =>
-{
-    const pagesNeeded = Math.ceil(allStudents.length / 10);
-    const paginationDiv = document.createElement('div');
-    paginationDiv.className = "pagination";
-    pageDiv.appendChild(paginationDiv);
-    const listOfLinks = document.createElement('ul');
-    paginationDiv.appendChild(listOfLinks);
-
-    for (let i = 0; i < pagesNeeded; i += 1)
-    {
-        let pageNumber = i + 1;
-        let listItem = document.createElement('li');
-        listOfLinks.appendChild(listItem);
-        let aItem = document.createElement('A');
-        listItem.appendChild(aItem);
-        aItem.textContent = pageNumber;
-        aItem.setAttribute("href", "#");
-    }
-    //possible answer: if (listOfLinks.firstChild !== null)
-    listOfLinks.firstChild.className = "active";
-    return pagesNeeded;
-}
-
-pageDiv.addEventListener('click', (event) => {
-    const pageClicked = event.target.textContent;
-
-    if (event.target.tagName == 'A')
-    {   
-        showPage(allStudents, pageClicked);
-
-        event.target.className = "active";
-        
-        for (let i = 0; i < allLinks.length; i += 1)
-        {
-            let oneLink = allLinks[i];
-            let text = oneLink.textContent;
-            console.log(text);
-            if (event.target.textContent !== text)
-            {
-                allLinks[i].classList.remove("active");
+            if (i >= first && i < last) {
+                student.style.display = "block";
+            }
+            else {
+                student.style.display = "none";
             }
         }
     }
 
-    if (event.target.className == "searching")
-    {
-        
-    }
-});
+    /*** 
+       appendPageLinks takes in the list of students, determines pages needed and sets up links at bottom of page
+       DOM elements are saved in the initial part
+       ****The extra credit I was unable to complete is to have links show the results of the search appropriately****
+    ***/
+    const appendPageLinks = (allStudents) => {
+        const pagesNeeded = Math.ceil(allStudents.length / 10);
+        const paginationDiv = document.createElement('div');
+        paginationDiv.className = "pagination";
+        pageDiv.appendChild(paginationDiv);
+        const listOfLinks = document.createElement('ul');
+        paginationDiv.appendChild(listOfLinks);
 
-/***
-https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
-***/
-input.addEventListener('input', (event) => {
-    const name = event.srcElement.value;
-    var resultingStudents;
-    const removalPagination = document.querySelector('.pagination');
-
-    if (removalPagination !== null) {
-        removalPagination.remove();
-    }
-
-    //for (let i = 0; i < allStudents.length; i += 1)
-    //{
-    //    let student = allNames[i].innerText;
-    //    if (student.includes(name) || name === "")
-    //    {
-    //        allStudents[i].style.display = "block";
-    //        resultingStudents.push(allStudents[i]);
-    //    }
-    //    else
-    //    {
-    //        allStudents[i].style.display = "none";
-    //    }
-    //}
-    resultingStudents = checkNames(name).slice(0);
-    if (resultingStudents.length > 0) //If there's search results
-    {
-        showPage(resultingStudents, 1);
-        appendPageLinks(resultingStudents);
-    }
-});
-
-const checkNames = (string) => {
-    var resultingStudents = [];
-
-    for (let i = 0; i < allStudents.length; i += 1) {
-        let student = allNames[i].innerText;
-        if (student.includes(string) || string === "") {
-            allStudents[i].style.display = "block";
-            resultingStudents.push(allStudents[i]);
+        for (let i = 0; i < pagesNeeded; i += 1) {
+            let pageNumber = i + 1;
+            let listItem = document.createElement('li');
+            listOfLinks.appendChild(listItem);
+            let aItem = document.createElement('A');
+            listItem.appendChild(aItem);
+            aItem.textContent = pageNumber;
+            aItem.setAttribute("href", "#");
         }
-        else {
-            allStudents[i].style.display = "none";
-        }
+        return pagesNeeded;
     }
-}
 
-showPage(allStudents, 1);
-appendPageLinks(allStudents);
+    /***
+        pageDiv eventlistener listens for click events throughout the page div
+        It reacts depending on the link, search, or reset button clicked
+        Link: Lets you view corresponding page with regard to link clicked
+        Search: Helps display 'No search results found' if no matching string
+        Reset: *Clears searches and search input and sets list back to default to allow user to search again
+               *Also removes the 'No search results found'
+    ***/
 
+    pageDiv.addEventListener('click', (event) => {
+        const pageClicked = event.target.textContent;
+        const name = input.value;
+
+        if (event.target.tagName == 'A')                // LINKS CLICK
+        {
+            showPage(allStudents, pageClicked);
+
+            event.target.className = "active";          // Sets clicked link to active
+
+            for (let i = 0; i < allLinks.length; i += 1) // Removes active class from non-clicked links
+            {
+                let oneLink = allLinks[i];
+                let text = oneLink.textContent;
+                if (event.target.textContent !== text) {
+                    allLinks[i].classList.remove("active");
+                }
+            }
+        }
+
+        if (event.target.textContent == "Search")       // SEARCH CLICK
+        {
+            var searchFound = false;
+            for (let i = 0; i < allStudents.length; i += 1) // Loops through students to see if there is matching name, if at least one is found, breaks
+            {                                               // If not found, appends 'No search results found' div
+                let student = allNames[i].innerText;
+                if (student.includes(name) || name === "") {
+                    searchFound = true;
+                    break;
+                }
+            }
+            if (!(searchFound)) {
+                const parent = allStudents.parentNode;
+                pageDiv.insertBefore(noResults, parent);
+            }
+        }
+
+        if (event.target.textContent == "Reset")        // RESET CLICK
+        {
+            const removalPagination = document.querySelector('.pagination'); // Helps remove current pagination
+            input.value = "";                                                // Resets value of input for search
+            if (removalPagination !== null) {
+                removalPagination.remove();
+            }
+            if (noResults !== null) {
+                noResults.remove();                                          // If no search results were found, removes statement
+            }
+            showPage(allStudents, 1);                                        // Recalls functions to set page back to default 
+            appendPageLinks(allStudents);
+        }
+    });
+
+    /***
+        Page used for input reference:
+        https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
+        Input eventlistener reacts every time a new letter is typed to or deleted from the search input
+        It then creates a new list of students to display based on that search and in real time
+    ***/
+    input.addEventListener('input', (event) => {
+        const name = event.srcElement.value;                // Saves value typed into the input
+        var resultingStudents = [];                         // Prepares variable for the new student list
+
+        const removalPagination = document.querySelector('.pagination');
+        if (removalPagination !== null)                     // Removes  current pagination links as search result numbers change
+        {
+            removalPagination.remove();
+        }
+
+        for (let i = 0; i < allStudents.length; i += 1)     // Goes through list of students and, if input matches a letter in student name,
+        {                                                   // adds to new list
+            let student = allNames[i].innerText;
+            if (student.includes(name) || name === "") {
+                allStudents[i].style.display = "block";
+                resultingStudents.push(allStudents[i]);
+            }
+            else {
+                allStudents[i].style.display = "none";
+            }
+        }
+        if (resultingStudents.length > 0)                   // Makes sure that there are search results before showing the new page
+        {
+            showPage(resultingStudents, 1);
+            appendPageLinks(resultingStudents);
+        }
+    });
+
+    /***
+        Calling functions to set up initial page
+    ***/
+    showPage(allStudents, 1);
+    appendPageLinks(allStudents);
+});
