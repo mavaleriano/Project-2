867daa5691df2f35856689770c7046b4e25f70fd
mavaleriano <miasvaleriano@gmail.com>
2019-08-07 00:22:30 -0400

    Partially completed
    Everything works except:
    the bottom pagination links aren't working correctly. That's all that's left!

--- js/script.js
+++ js/script.js
@@ -9,42 +9,153 @@ FSJS project 2 - List Filter and Pagination
 /*** 
    Add your global variables that store the DOM elements you will 
    need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
 ***/
-
-
+const allStudents = document.querySelectorAll('li');
+const pageDiv = document.querySelector('.page');
+const allNames = document.querySelectorAll('h3');
+const allLinks = document.getElementsByTagName('A');
+const header = document.querySelector('.page-header.cf');
+const search = document.createElement('div');
+const noResults = document.createElement('div');
+noResults.innerHTML = "<h3>No search results found..</h3>";
+search.className = "student-search";
+header.appendChild(search);
+const input = document.createElement('input');
+input.placeholder = "Search for students..";
+search.appendChild(input);
+const searchButton = document.createElement('button');
+searchButton.textContent = "Search";
+searchButton.className = "searching"
+search.appendChild(searchButton);
 
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
 ***/
-
-
-
+const showPage = (list, page) =>
+{
+    for (let i = 0; i < list.length; i += 1)
+    {
+        let student = list[i];
+        let first = (page - 1) * 10; //sets the first student that should show up
+        let last = page * 10;        //sets last student that should show up
+      
+        if (i >= first && i < last)
+        {
+            student.style.display = "block";
+        }
+        else
+        {
+            student.style.display = "none";
+        }
+    }
+}
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+const appendPageLinks = (allStudents) =>
+{
+    const pagesNeeded = Math.ceil(allStudents.length / 10);
+    const paginationDiv = document.createElement('div');
+    paginationDiv.className = "pagination";
+    pageDiv.appendChild(paginationDiv);
+    const listOfLinks = document.createElement('ul');
+    paginationDiv.appendChild(listOfLinks);
 
+    for (let i = 0; i < pagesNeeded; i += 1)
+    {
+        let pageNumber = i + 1;
+        let listItem = document.createElement('li');
+        listOfLinks.appendChild(listItem);
+        let aItem = document.createElement('A');
+        listItem.appendChild(aItem);
+        aItem.textContent = pageNumber;
+        aItem.setAttribute("href", "#");
+    }
+    //possible answer: if (listOfLinks.firstChild !== null)
+    listOfLinks.firstChild.className = "active";
+    return pagesNeeded;
+}
 
+pageDiv.addEventListener('click', (event) => {
+    const pageClicked = event.target.textContent;
 
+    if (event.target.tagName == 'A')
+    {   
+        showPage(allStudents, pageClicked);
 
+        event.target.className = "active";
+        
+        for (let i = 0; i < allLinks.length; i += 1)
+        {
+            let oneLink = allLinks[i];
+            let text = oneLink.textContent;
+            console.log(text);
+            if (event.target.textContent !== text)
+            {
+                allLinks[i].classList.remove("active");
+            }
+        }
+    }
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+    if (event.target.className == "searching")
+    {
+        
+    }
+});
+
+/***
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
+***/
+input.addEventListener('input', (event) => {
+    const name = event.srcElement.value;
+    var resultingStudents;
+    const removalPagination = document.querySelector('.pagination');
+
+    if (removalPagination !== null) {
+        removalPagination.remove();
+    }
+
+    //for (let i = 0; i < allStudents.length; i += 1)
+    //{
+    //    let student = allNames[i].innerText;
+    //    if (student.includes(name) || name === "")
+    //    {
+    //        allStudents[i].style.display = "block";
+    //        resultingStudents.push(allStudents[i]);
+    //    }
+    //    else
+    //    {
+    //        allStudents[i].style.display = "none";
+    //    }
+    //}
+    resultingStudents = checkNames(name).slice(0);
+    if (resultingStudents.length > 0) //If there's search results
+    {
+        showPage(resultingStudents, 1);
+        appendPageLinks(resultingStudents);
+    }
+});
+
+const checkNames = (string) => {
+    var resultingStudents = [];
+
+    for (let i = 0; i < allStudents.length; i += 1) {
+        let student = allNames[i].innerText;
+        if (student.includes(string) || string === "") {
+            allStudents[i].style.display = "block";
+            resultingStudents.push(allStudents[i]);
+        }
+        else {
+            allStudents[i].style.display = "none";
+        }
+    }
+}
+
+showPage(allStudents, 1);
+appendPageLinks(allStudents);
+
